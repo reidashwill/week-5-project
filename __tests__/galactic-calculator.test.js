a990c307a7fac0ddfc423ff4944be8c37d2fc47f
Reid Ashwill <reidashwill@gmail.com>
2020-04-17 09:13:45 -0700

    test to verify that age is a number is passing

--- __tests__/galactic-calculator.test.js
+++ __tests__/galactic-calculator.test.js
@@ -1,9 +1,9 @@
-import { User } from "../src/user.js"
+import { User } from "./../src/user.js"
 
 describe("User", () => {
 
-  TextDecoderStream("should verify that the age property of user is a number", () => {
-    const user1 = new User("32");
-    expect(typeof(user1.age)).toEqual("string");
+  test("should verify that the age property of user is a number", () => {
+    const user1 = new User(35);
+    expect(typeof(user1.age)).toEqual("number");
   })
-})
+}) 
