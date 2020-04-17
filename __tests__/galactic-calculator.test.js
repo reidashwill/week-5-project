ad2b1099974c8a537a2ffc1b210f185037efcc18
Reid Ashwill <reidashwill@gmail.com>
2020-04-17 09:59:38 -0700

    updated constructor test continues working with addition of life expectancy property.

--- __tests__/galactic-calculator.test.js
+++ __tests__/galactic-calculator.test.js
@@ -2,28 +2,29 @@ import { User } from "./../src/user.js"
 
 describe("User", () => {
 
-  test("should verify that the age property of user is a number", () => {
-    const user1 = new User(35);
+  test("should verify that User constructor is a creating age and life expectancy properties", () => {
+    const user1 = new User(35, 85);
     expect(typeof(user1.age)).toEqual("number");
+    expect(typeof(user1.lifeExp)).toEqual("number");
   })
 
   test("should verify that mercConverter is working to convert earth years into mercury years", () => {
-    const user2 = new User(35);
+    const user2 = new User(35, 85);
     expect(user2.mercConverter()).toEqual(145);
   })
 
   test("should verify that venusConverter is working to convert earth years into mercury years", () => {
-    const user3 = new User(35);
+    const user3 = new User(35, 85);
     expect(user3.venusConverter()).toEqual(56);
   })
 
   test("should verify that marsConverter is working to convert earth years into mercury years", () => {
-    const user4 = new User(35);
+    const user4 = new User(35, 85);
     expect(user4.marsConverter()).toEqual(18);
   })
 
   test("should verify that jupiterConverter is working to convert earth years into mercury years", () => {
-    const user5 = new User(35);
+    const user5 = new User(35, 85);
     expect(user5.jupiterConverter()).toEqual(2);
   })
 
--- src/user.js
+++ src/user.js
@@ -1,6 +1,7 @@
 export class User {
-  constructor(age) {
+  constructor(age, lifeExp) {
     this.age = age;
+    this.lifeExp = lifeExp;
   }
 
   mercConverter() {
